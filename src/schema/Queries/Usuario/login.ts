aafb342b5a5ca15ff2e6ffd6f3748745888a3cda
MultiCuentas-Gonza <thegonzalo9000@gmail.com>
2023-01-07 17:47:17 -0300

    modificado para redsocial y recuperarContraseña

--- src/schema/Queries/Usuario/login.ts
+++ src/schema/Queries/Usuario/login.ts
@@ -5,7 +5,29 @@ import { JWT_SECRET } from "../../../config";
 import { getUsuarioById } from "../../../ORM_Queries/Usuario/getUsuarioById";
 import { login } from "../../../ORM_Queries/Usuario/login";
 import { SendUsuario } from "../../../SendTypes/SendUsuario";
+import { loginByRedSocial } from "../../../ORM_Queries/Usuario/loginByRedSocial";
 
+async function LoginByRedSocial(nombre: string, correo: string, contrasenia: string) 
+{
+    const msj = new SendUsuario()
+
+    try {
+        const usuario = await loginByRedSocial(nombre , correo, contrasenia)
+
+        const id_usuario: string = usuario[0].id.toString()
+
+        msj.message = 'USUARIO LOGUEADO CORRECTAMENTE!!'
+        msj.accessToken = sign(id_usuario, JWT_SECRET);
+        msj.success = true;
+        msj.usuario = usuario[0];
+      
+        return msj;
+    } catch (err: any) {
+        msj.message = err
+        msj.success = false
+        return msj;
+    }    
+}
 
 async function GetUsuarioByCorreoYPassword(args: any): Promise<SendUsuario> 
 {
@@ -65,15 +87,18 @@ async function GetUsuarioByToken(tokenUser: string): Promise<SendUsuario>
 
 export async function selectLoginType(args:  any)
 {
-    if (args.correo != '' && args.contrasenia != '')
+    if(args.nombre != '' && (args.correo != '' && (args.contrasenia != '' && args.contrasenia != null)))
+    {
+        return await LoginByRedSocial(args.nombre, args.correo, args.contrasenia)
+    }
+    else if (args.correo != '' && (args.contrasenia != '' && args.contrasenia != null))
     {
         return await GetUsuarioByCorreoYPassword(args)
     }
     else if(args.tokenUser != '')
     {
         return await GetUsuarioByToken(args.tokenUser)
-    }
+    } 
     
     return new SendUsuario()
 }
-
